bcb7d9c77e456a5b91e9295f57559d35de1152e6
ali-shokoohi23 <alibest330@gmail.com>
2024-08-05 13:12:18 +0330

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    }); 
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
